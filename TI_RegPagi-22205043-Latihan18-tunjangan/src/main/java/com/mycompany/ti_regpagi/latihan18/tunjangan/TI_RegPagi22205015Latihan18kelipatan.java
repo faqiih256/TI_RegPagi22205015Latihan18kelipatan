e83d2134a6797523c2544b4e46f5295dd90c7573
faqiih256 <145763727+faqiih256@users.noreply.github.com>
2023-10-26 22:30:44 +0700

    Add files via upload

---
+++ TI_RegPagi-22205043-Latihan18-tunjangan/src/main/java/com/mycompany/ti_regpagi/latihan18/tunjangan/TI_RegPagi22205015Latihan18kelipatan.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ti_regpagi.latihan18.tunjangan;
+
+
+/**
+ *Nama          : Ahmad Nurfaqih
+ * Nim          : 22205015
+ * prodi        :Teknik Informatia
+ * Deskripsi    : program ini berisi kelipanan 3.5 sampai 35
+ * @author 62895
+ */
+public class TI_RegPagi22205015Latihan18kelipatan {
+
+    public static void main(String[]args){
+        System.out.println("===LATIHAN 18 KELIPATAN===\n");
+        double i = 3.5;
+        while (i <= 35) {
+            System.out.println(i);
+            i=i+3.5;
+        }
+    }
+}
